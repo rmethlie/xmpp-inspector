0790ab4b2f162bc1b6db4e875c12139fc6921816
Paul Guerra <paul.guerra@thomsonreuters.com>
2014-04-01 12:36:29 -0400

    further implemented require.js

--- panel/javascripts/panel.js
+++ panel/javascripts/panel.js
@@ -1,62 +1,63 @@
 
 require(["config"], function() {
-  require(["jquery", "underscore", "backbone"], function(jquery, underscore, backbone) {
-      console.log("init");
+  require(["app"], function(app) {
+    console.log("init", app);
+    try{
+      var
+      httpBind = /http-bind/i,
+      emDevToolsBackground = chrome.runtime.connect({
+          name: "background"
+      });
+
+      function logToPage( message, error, panelBody ){
+        
+        $(panelBody).append( "<div class='request'><pre>" + message + "</pre></div>" );  
+        
+        if( error ){
+          emDevToolsBackground.postMessage({
+            error: message
+          })
+        }else{
+          emDevToolsBackground.postMessage({
+            log: message
+          })
+        }
+      }
+
+        
+      chrome.devtools.network.onRequestFinished.addListener(function(packet){
+        try{
+          var
+            body = $("#xmpp-inspector-panel")[0],
+            $body = $(body);
+
+          
+          if( httpBind.test( packet.request.url ) ){
+            logToPage( packet.request.url, null, $body );
+
+            packet.getContent( function(contents){
+
+              try{
+                $body.append( "<div>test</div>" );
+                contents = $.parseXML(contents);
+                $(contents).format({method:'xml'});//.replace( /</g, "&lt;" ).replace( />/g, "&gt;" );
+                $body.append( contents.childNodes[0] );
+                
+              }catch( eee ){
+                logToPage( eee.stack, true );
+              }
+            });
+          }
+        }catch( ee ){
+          logToPage( ee.stack, true );
+        }
+      });
+
+    }catch( e ){
+      logToPage( e.stack, true );
+    }
 
   });
 });
 /*
-try{
-  var
-  httpBind = /http-bind/i,
-  emDevToolsBackground = chrome.runtime.connect({
-      name: "background"
-  });
-
-  function logToPage( message, error, panelBody ){
-    
-    panelBody.append( "<div class='request'><pre>" + message + "</pre></div>" );  
-    
-    if( error ){
-      emDevToolsBackground.postMessage({
-        error: message
-      })
-    }else{
-      emDevToolsBackground.postMessage({
-        log: message
-      })
-    }
-  }
-
-    
-  chrome.devtools.network.onRequestFinished.addListener(function(packet){
-    try{
-      var
-        body = $(document.body)[0],
-        $body = $(body);
-      logToPage( packet.request.url, null, $body );
-      if( httpBind.test( packet.request.url ) ){
-
-        
-        packet.getContent( function(contents){
-
-          try{
-            $body.append( "<div>test</div>" );
-            contents = $.parseXML(contents);
-            $(contents).format({method:'xml'});//.replace( /</g, "&lt;" ).replace( />/g, "&gt;" );
-            $body.append( contents.childNodes[0] );
-            
-          }catch( eee ){
-            logToPage( eee.stack, true );
-          }
-        });
-      }
-    }catch( ee ){
-      logToPage( ee.stack, true );
-    }
-  });
-
-}catch( e ){
-  logToPage( e.stack, true );
-}
 */
