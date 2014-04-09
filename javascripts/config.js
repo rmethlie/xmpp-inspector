a34f3240f148a86951521e284c605870468be1a3
Paul Guerra <paul.guerra@thomsonreuters.com>
2014-04-09 19:46:43 -0400

    a shared require.config file of all the processes

---
+++ javascripts/config.js
@@ -0,0 +1,19 @@
+require.config({
+    baseUrl: "/javascripts/",
+    paths: {
+        underscore: "bower_components/underscore/underscore",
+        jquery: "bower_components/jquery/dist/jquery.min",
+        backbone: "bower_components/backbone/backbone",
+        text: "bower_components/requirejs-text/text",
+        codemirror: "bower_components/codemirror/",
+        beautifier: "bower_components/js-beautify/js/lib",
+        BaseView: "views/Base.view",
+        BaseModel: "models/Base.model",
+        BaseListener: "models/BaseListener.model",
+        Packet: "models/Packet.model",
+        StreamView: "views/Stream.view",
+        XMPPStreamView: "views/XMPPStream.view",
+        Packet: "models/Packet.model",
+        NetworkRequest: "models/NetworkRequest.model"
+    }
+});
--- javascripts/panel.js
+++ javascripts/panel.js
@@ -1,25 +1,12 @@
-require.config({
-  paths: {
-    underscore: "/javascripts/bower_components/underscore/underscore",
-    jquery: "/javascripts/bower_components/jquery/dist/jquery.min",
-    backbone: "/javascripts/bower_components/backbone/backbone",
-    text: "/javascripts/bower_components/requirejs-text/text",
-    codemirror: "bower_components/codemirror/",
-    beautifier: "bower_components/js-beautify/js/lib",
-    BaseView: "/javascripts/views/Base.view",
-    BaseModel: "/javascripts/models/Base.model",
-    BaseListener: "/javascripts/models/BaseListener.model",
-    StreamView: "/javascripts/views/Stream.view",
-    XMPPStreamView: "/javascripts/views/XMPPStream.view"
-  }
-});
+
+require(["config"], function() {
 require(["app", 
     "BaseListener", 
     "XMPPStreamView"], function(app, BaseListener, XMPPStreamView) {
-    console.log("init", app);
-    
+    console.log("init", app);    
 
     var stream = new XMPPStreamView();
     console.log("stream:", stream);
 
   });
+});
