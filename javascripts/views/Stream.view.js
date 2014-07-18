b3abc84592229497e116fe6f3063609e26f4b9c9
Paul Guerra <paul.guerra@thomsonreuters.com>
2014-07-18 15:07:44 -0400

    using search cursor programmatically

--- javascripts/views/Stream.view.js
+++ javascripts/views/Stream.view.js
@@ -9,6 +9,47 @@ define(['BaseView',
   
   var format = require('beautifier/beautify-html');
   var CodeMirror = require('codemirror/lib/codemirror');
+  
+  function SearchState() {
+    this.posFrom = this.posTo = this.query = null;
+    this.overlay = null;
+  }
+
+  function queryCaseInsensitive(query) {
+    return typeof query == "string" && query == query.toLowerCase();
+  }
+
+  function searchOverlay(query, caseInsensitive) {
+    var startChar;
+    if (typeof query == "string") {
+      startChar = query.charAt(0);
+      query = new RegExp("^" + query.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&"),
+                         caseInsensitive ? "i" : "");
+    } else {
+      query = new RegExp("^(?:" + query.source + ")", query.ignoreCase ? "i" : "");
+    }
+    return {token: function(stream) {
+      if (stream.match(query)) return "searching";
+      while (!stream.eol()) {
+        stream.next();
+        if (startChar && !caseInsensitive)
+          stream.skipTo(startChar) || stream.skipToEnd();
+        if (stream.match(query, false)) break;
+      }
+    }};
+  }
+
+
+  function parseQuery(query) {
+    var isRE = query.match(/^\/(.*)\/([a-z]*)$/);
+    if (isRE) {
+      query = new RegExp(isRE[1], isRE[2].indexOf("i") == -1 ? "" : "i");
+      if (query.test("")) query = /x^/;
+    } else if (query == "") {
+      query = /x^/;
+    }
+    return query;
+  }
 
   return BaseView.extend({
     
@@ -31,10 +72,47 @@ define(['BaseView',
       console.log("[StreamView] initialize");
       this.render();
       this.dataStream = CodeMirror.fromTextArea(document.getElementById("dataStream"), this.dataStreamConfig);
-      
-      this.dataStream.execCommand("findNext");
+      this.find("key");
+      // this.dataStream.execCommand("findNext");
       this.addlisteners(options);
-      // this.model.connect();
+    },
+    
+    find: function(query, rev){
+      var _this = this;
+      var state = this.getSearchState();
+      this.dataStream.operation( function() {
+        if (!query || state.query) return;
+        state.query = parseQuery(query);
+        _this.dataStream.removeOverlay(state.overlay, queryCaseInsensitive(state.query));
+        state.overlay = searchOverlay(state.query, queryCaseInsensitive(state.query));
+        _this.dataStream.addOverlay(state.overlay);
+        state.posFrom = state.posTo = _this.dataStream.getCursor();
+        _this.findNext(_this.dataStream, rev);
+      });
+    },
+
+    findNext: function(cm, rev) {
+      var _this = this;
+      function getSearchCursor(cm, query, pos) {
+        // Heuristic: if the query string is all lowercase, do a case insensitive search.
+        return cm.getSearchCursor(query, pos, queryCaseInsensitive(query));
+      };
+
+      this.dataStream.operation(function() {
+        var state = _this.getSearchState(_this.dataStream);
+        var cursor = getSearchCursor(_this.dataStream, state.query, rev ? state.posFrom : state.posTo);
+        if (!cursor.find(rev)) {
+          cursor = getSearchCursor(_this.dataStream, state.query, rev ? CodeMirror.Pos(cm.lastLine()) : CodeMirror.Pos(cm.firstLine(), 0));
+          if (!cursor.find(rev)) return;
+        }
+        _this.dataStream.setSelection(cursor.from(), cursor.to());
+        _this.dataStream.scrollIntoView({from: cursor.from(), to: cursor.to()});
+        state.posFrom = cursor.from(); state.posTo = cursor.to();
+      });
+    },
+
+    getSearchState: function () {
+      return this.dataStream.state.search || (this.dataStream.state.search = new SearchState());
     },
 
     addlisteners: function(options){
