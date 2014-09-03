a29c9d35cc63a10a51879c6759883818ce792f31
Paul Guerra <paul.guerra@thomsonreuters.com>
2014-09-03 19:09:54 -0400

    created basic template files

--- javascripts/views/StreamsManager.view.js
+++ javascripts/views/StreamsManager.view.js
@@ -1,13 +1,22 @@
-define(["BaseView"], function(BaseView) {
+define(["BaseView", 'text!templates/streamsManager.template.html',], function(BaseView, streamsManagerTemplate) {
   "use strict";
 
   return BaseView.extend({
     
     el : "#stream-manager",
 
+    template: _.template(streamsManagerTemplate),
+
     initialize: function(){
       this.render();
     },
 
+    render: function(options){
+      if(!options)
+        options = {};
+
+      this.$el.html(this.template(options));
+
+    },
   });
 });
